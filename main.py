fa05fb5f48e24597b683505e2e6c80aec090888f
Jean-Marc Ougier <jmougier5@gmail.com>
2021-11-09 21:02:27 +0100

    Avec fenetre tkinter et affichage de l'échiquier

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
-
+import tkinter
+from tkinter import Label, Tk, Pack
 from src.class_echiquier import Echiquier
 from src.class_piece import Piece
+from PIL import Image, ImageTk
+
+fenetre = Tk()
+fenetre.geometry("825x834")
+champ_label = Label(fenetre, text="Les échecs c'est la vie !")
+champ_label.pack()
 
 
-ech=Echiquier()
+ech = Echiquier()
 
-#test
+# test
 ech.new_game('blanc')
-ech.trace()
+image1=ech.trace().resize((800,800))
+test=ImageTk.PhotoImage(image1)
 
+label1 = tkinter.Label(image=test)
+# label1.image = test
 
+label1.place(x=10,y=20)
 
-
-
-
-
-
+fenetre.mainloop()
