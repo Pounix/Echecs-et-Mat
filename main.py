e69c63cdd91fe19f80ae77eefa3732abd6241017
Jean-Marc Ougier <jmougier5@gmail.com>
2021-11-16 21:32:54 +0100

    Début de jeu qui fonctionne !!

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
+import sys
+sys.path.append(r'F:\OneDrive\Coding\Python\Python3\EchecEtMat\src')
 
 import tkinter
-
-
 from tkinter import Button, Label, Tk, Pack, Frame, Canvas
 from tkinter.constants import NW
 from src.class_echiquier import Echiquier
 from src.class_piece import Piece
 from PIL import Image, ImageTk
 
+              
 class Plateau(Tk):
-
+    coup_choisi=''
+    ech_coups_possibles={}
+    
     def __init__(self):
         Tk.__init__(self)
         self.x = self.y = 0
@@ -18,33 +21,33 @@ class Plateau(Tk):
         self.height=800
         self.canvas = Canvas(self, width=self.width, height=self.height, cursor="cross")
         self.canvas.pack(side="top", fill="both", expand=True)
-
         self.ech = Echiquier()
         self.ech.new_game()
-
         self.image = self.ech.trace().resize((self.width, self.height))
-
         self.photo = ImageTk.PhotoImage(self.image)
         self.canvas.create_image((0, 0), anchor="nw", image=self.photo)
-
         self.bind('<Button-1>', self.update)
 
     def update(self, event):
         ca = self.ech.ma_case(event.x,event.y)
         # Trouver s'il y a une pièce sur cette case...
-        try:
-            p=self.ech.pieces[ca]
-        except KeyError:
-            p=''
-        
-        if isinstance(p,Piece):
+        p = self.ech.pieces[ca] if ca in self.ech.pieces.keys() else ''
+        cur = self.ech.pieces[ca+'p'] if (ca+'p') in self.ech.pieces.keys() else ''
+        if p!='':
+            # breakpoint()
             self.ech.cleanCursor()
-            self.ech.pieces[ca+'c']=(Piece(couleur="cursor", valeur="Select", type=False,case_précédente=ca))  # fichier image = valeur_couleur
-
-            # for c in cj:
-            #     self.ech.echiquier.append(Piece(couleur="cursor", valeur="Possible", colonne=c[0], ligne=c[1],type='cursor'))
-            
-            
+            self.ech_coups_possibles=self.ech.coups_jouables(ca)
+            v = 'Bloqué' if self.ech_coups_possibles=={} else 'Select'
+            self.ech.pieces[ca+'c']=Piece(couleur="cursor", valeur=v, type=False,case_précédente=ca) # fichier image = valeur_couleur
+            v='Possible'
+            for c in self.ech_coups_possibles.keys():
+                self.ech.pieces[c+'p']=Piece(couleur="cursor", valeur=v, type=False,case_précédente=c)
+            self.image = self.ech.trace().resize((self.width, self.height))
+            self.photo = ImageTk.PhotoImage(self.image)
+            self.canvas.create_image((0, 0), anchor="nw", image=self.photo)
+        elif cur!='':
+            self.ech.pieces=self.ech_coups_possibles[ca]
+            self.ech.cleanCursor()
             self.image = self.ech.trace().resize((self.width, self.height))
             self.photo = ImageTk.PhotoImage(self.image)
             self.canvas.create_image((0, 0), anchor="nw", image=self.photo)
